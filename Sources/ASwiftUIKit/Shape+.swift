a61b14e90cfede811d03b6ff95b5a428001925d2
iAugus <iAugux@gmail.com>
2023-07-18 20:47:43 +0800

    add compatipleGradient

---
+++ Sources/ASwiftUIKit/Shape+.swift
@@ -0,0 +1,15 @@
+// Created by Augus on 7/18/23
+// Copyright © 2023 Augus <iAugux@gmail.com>
+
+import SwiftUI
+
+public extension Shape {
+    @ViewBuilder
+    func compatipleGradient(_ color: Color) -> some View {
+        if #available(iOS 16.0, macOS 13.0, watchOS 9.0, *) {
+            self.fill(color.gradient)
+        } else {
+            fill(LinearGradient(gradient: .init(colors: [color.opacity(0.6), color]), startPoint: .top, endPoint: .bottom))
+        }
+    }
+}
