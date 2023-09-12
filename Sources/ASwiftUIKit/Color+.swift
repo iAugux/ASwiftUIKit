3e51d40e2b5076771e9efd589fec1ffcc5feca46
iAugus <iAugux@gmail.com>
2023-09-12 14:08:39 +0800

    update

--- Sources/ASwiftUIKit/Color+.swift
+++ Sources/ASwiftUIKit/Color+.swift
@@ -20,6 +20,14 @@ public extension PlatformColor {
 }
 
 public extension Color {
+    private init(_uiColor: UIColor) {
+        if #available(iOS 15.0, *) {
+            self.init(uiColor: _uiColor)
+        } else {
+            self.init(_uiColor)
+        }
+    }
+
     init(hex: UInt, alpha: Double = 1) {
         self.init(
             .sRGB,
@@ -42,6 +50,82 @@ public extension Color {
     static let systemBlack: Color = .init(UIColor(dynamicProvider: { traitCollection -> UIColor in
         return traitCollection.userInterfaceStyle == .dark ? .white : .black
     }))
+
+    /// The color for the main background of your interface.
+    /// - Use this color for standard table views and designs that have a white primary background in a light environment.
+    static var systemBackground: Color {
+        .init(_uiColor: .systemBackground)
+    }
+
+    /// The color for content layered on top of the main background.
+    /// - Use this color for standard table views and designs that have a white primary background in a light environment.
+    static var secondarySystemBackground: Color {
+        .init(_uiColor: .secondarySystemBackground)
+    }
+
+    /// The color for content layered on top of secondary backgrounds.
+    /// - Use this color for standard table views and designs that have a white primary background in a light environment.
+    static var tertiarySystemBackground: Color {
+        .init(_uiColor: .tertiarySystemBackground)
+    }
+    
+    /// The color for the main background of your grouped interface.
+    /// - Use this color for grouped content, including table views and platter-based designs.
+    static var systemGroupedBackground: Color {
+        .init(_uiColor: .systemGroupedBackground)
+    }
+
+    /// The color for content layered on top of the main background of your grouped interface.
+    /// - Use this color for grouped content, including table views and platter-based designs.
+    static var secondarySystemGroupedBackground: Color {
+        .init(_uiColor: .secondarySystemGroupedBackground)
+    }
+    
+    /// The color for content layered on top of secondary backgrounds of your grouped interface.
+    /// - Use this color for grouped content, including table views and platter-based designs.
+    static var tertiarySystemGroupedBackground: Color {
+        .init(_uiColor: .tertiarySystemGroupedBackground)
+    }
+    
+    /// An overlay fill color for thin and small shapes.
+    /// - Use system fill colors for items situated on top of an existing background color. System fill colors incorporate transparency to allow the background color to show through.
+    /// - Use this color to fill thin or small shapes, such as the track of a slider.
+    static var systemFill: Color {
+        .init(_uiColor: .systemFill)
+    }
+    
+    /// An overlay fill color for medium-size shapes.
+    /// - Use system fill colors for items situated on top of an existing background color. System fill colors incorporate transparency to allow the background color to show through.
+    /// - Use this color to fill medium-size shapes, such as the background of a switch.
+    static var secondarySystemFill: Color {
+        .init(_uiColor: .secondarySystemFill)
+    }
+    
+    /// An overlay fill color for large shapes.
+    /// - Use system fill colors for items situated on top of an existing background color. System fill colors incorporate transparency to allow the background color to show through.
+    /// - Use this color to fill large shapes, such as input fields, search bars, or buttons.
+    static var tertiarySystemFill: Color {
+        .init(_uiColor: .tertiarySystemFill)
+    }
+    
+    /// An overlay fill color for large areas that contain complex content.
+    /// - Use system fill colors for items situated on top of an existing background color. System fill colors incorporate transparency to allow the background color to show through.
+    /// - Use this color to fill large areas that contain complex content, such as an expanded table cell.
+    static var quaternarySystemFill: Color {
+        .init(_uiColor: .quaternarySystemFill)
+    }
+    
+    /// The nonadaptable system color for text on a dark background.
+    /// - This color doesn’t adapt to changes in the underlying trait environment.
+    static var lightText: Color {
+        .init(_uiColor: .lightText)
+    }
+    
+    /// The nonadaptable system color for text on a light background.
+    /// - This color doesn’t adapt to changes in the underlying trait environment.
+    static var darkText: Color {
+        .init(_uiColor: .darkText)
+    }
 #endif
 }
 
