d82c3a45ac592616d0a7fbc57d8137cdd877d46d
iAugus <iAugux@gmail.com>
2025-08-15 22:12:34 +0800

    -

--- Sources/ASwiftUIKit/CompatibleGlassEffect.swift
+++ Sources/ASwiftUIKit/CompatibleGlassEffect.swift
@@ -3,12 +3,13 @@
 
 import SwiftUI
 
-@available(iOS 14.0, *)
+@available(iOS 14.0, macOS 11.0, watchOS 7.0, *)
+@available(visionOS, unavailable)
 struct CompatibleGlassEffect: ViewModifier {
     func body(content: Content) -> some View {
         content
             .if(true) {
-                if #available(iOS 26.0, watchOS 26.0, *) {
+                if #available(iOS 26.0, macOS 26.0, watchOS 26.0, tvOS 26.0, *) {
                     $0.glassEffect()
                 } else {
                     $0
@@ -17,7 +18,8 @@ struct CompatibleGlassEffect: ViewModifier {
     }
 }
 
-@available(iOS 14.0, *)
+@available(iOS 14.0, macOS 11.0, watchOS 7.0, *)
+@available(visionOS, unavailable)
 extension View {
     func compatibleGlassEffect() -> some View {
         self.modifier(CompatibleGlassEffect())
